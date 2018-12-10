4108242ea71ce55269ffba1af2c0dfd675c22e6d
Jordy Moos <jordymoos@gmail.com>
2018-12-11 00:36:36 +0100

    Chopping carrots in code

---
+++ code/src/main/java/com/ing/baker/tutorials/snippets/ChopCarrot.java
@@ -0,0 +1,17 @@
+package com.ing.baker.tutorials.snippets;
+
+public class ChopCarrot {
+
+    public class ChoppedCarrot {
+
+    }
+
+    public ChoppedCarrot chop(int carrot, String knife) {
+        // Do awesome chopping job
+        return new ChoppedCarrot();
+    }
+}
+
+
+
+
