2ba960c8a69b913f6a9e06d735f96625926f2ebf
Jordy Moos <jordymoos@gmail.com>
2018-12-11 12:56:45 +0100

    Adding more slides

--- code/src/main/java/com/ing/baker/tutorials/App.java
+++ code/src/main/java/com/ing/baker/tutorials/App.java
@@ -1,5 +1,7 @@
 package com.ing.baker.tutorials;
 
+import com.ing.baker.runtime.java_api.JBaker;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,16 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+
+
+
+
+
+
+
+
+
+
     }
 }
